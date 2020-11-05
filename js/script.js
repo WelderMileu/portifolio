6a772c31fe90b60b6d3e12f83d4f4fe234a577e2
weldermileu <welderfelix21@gmail.com>
2020-11-05 14:47:54 -0300

    alterando stilo do menu, mudando a fonte, e estilizado os cards de apresentação

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 function navigation() {
     const content = document.querySelector('.fixado');
+    const iconMenu = document.querySelector('.icon-nav');
     
     const position = window.scrollY;
 
     if(position > 400) {
         content.classList.remove('absolute')
         content.classList.add('bg-white', 'fixed', 'shadow-lg')
-        content.classList.add('nav-fluid')
+        iconMenu.classList.add('text-teal-900')
     } else {
         content.classList.add('absolute')
-        content.classList.remove('bg-white', 'fixed', 'shadow-lg')      
-        content.classList.remove('nav-fluid')
+        content.classList.remove('bg-white', 'fixed', 'shadow-lg')
+        iconMenu.classList.remove('text-teal-900')      
     }
 }
 
